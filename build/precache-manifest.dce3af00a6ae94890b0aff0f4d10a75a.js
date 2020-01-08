91f7e90caca9b1d5ba70c3f2c90594f52d99e653
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2020-01-07 18:09:21 -0600

    adding build folder

---
+++ build/precache-manifest.dce3af00a6ae94890b0aff0f4d10a75a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63534c4b6e471003c41e886689db0ce0",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "4c813b53546c297fccdf",
+    "url": "/clicky-game/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "be743ff53266290e994a",
+    "url": "/clicky-game/static/js/2.8364b65b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.8364b65b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c813b53546c297fccdf",
+    "url": "/clicky-game/static/js/main.083b5879.chunk.js"
+  },
+  {
+    "revision": "76da5bcc0e2cfb61c97a",
+    "url": "/clicky-game/static/js/runtime-main.39dcd64a.js"
+  }
+]);
